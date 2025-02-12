77a2f3d36300c3ebcf6e6bf8dc3e7773083c6af0
julionevadod <julionevadod@gmail.com>
2025-02-12 18:11:09 +0100

    Simplified the network

--- src/continuous_control/actor.py
+++ src/continuous_control/actor.py
@@ -1,32 +1,22 @@
 import torch
 from torch import nn
+from torch.nn import functional as f
 
 
 class Actor(nn.Module):
-    def __init__(self, input_size: int, output_size: int, fc1_size: int = 400, fc2_size: int = 300):
+    def __init__(self, input_size: int, output_size: int, fc1_size: int = 128, fc2_size: int = 64):
         super().__init__()
 
         self.output_size = output_size
-
-        self.net = nn.Sequential(
-            nn.Linear(input_size, fc1_size, dtype=torch.float32),
-            nn.ReLU(),
-            nn.Linear(fc1_size, fc2_size, dtype=torch.float32),
-            nn.ReLU(),
-            nn.Linear(fc2_size, output_size, dtype=torch.float32),
-            nn.Tanh(),
-        )
+        self.bn0 = nn.BatchNorm1d(input_size)
+        self.fc1 = nn.Linear(input_size, fc1_size, dtype=torch.float32)
+        self.bn1 = nn.BatchNorm1d(fc1_size)
+        self.fc2 = nn.Linear(fc1_size, fc2_size, dtype=torch.float32)
+        self.bn2 = nn.BatchNorm1d(fc2_size)
+        self.fc3 = nn.Linear(fc2_size, output_size, dtype=torch.float32)
 
     def forward(self, state):
-        return self.net(state)
-
-    def act(self, state):
-        return self.forward(state)  # .squeeze()
-        # means = output[torch.arange(0, self.output_size, 2)]  # .clip(min=-1, max=1)
-        # stds = output[torch.arange(1, self.output_size, 2)]  # .clip(min=0)
-        # m = torch.distributions.Normal(means, stds)
-        # action = m.sample()
-        # log_proba = m.log_prob(action)
-        # action = torch.normal(means, stds)
-        # log_proba = self.get_log_proba(action, means, stds) #log proba results is the same as sampling from torch.distributions
-        # return action, log_proba
+        fc1_output = f.relu(self.fc1(state))
+        fc2_output = f.relu(self.fc2(fc1_output))
+        fc3_output = f.tanh(self.fc3(fc2_output))
+        return fc3_output
--- src/continuous_control/critic.py
+++ src/continuous_control/critic.py
@@ -4,9 +4,10 @@ from torch.nn import functional as f
 
 
 class Critic(nn.Module):
-    def __init__(self, input_size, action_size, output_size, fc1: int = 400, fc2: int = 300):
+    def __init__(self, input_size, action_size, output_size, fc1: int = 128, fc2: int = 64):
         super().__init__()
 
+        self.bn0 = nn.BatchNorm1d(input_size)
         self.fc1 = nn.Linear(input_size, fc1, dtype=torch.float32)
         self.fc2 = nn.Linear(fc1 + action_size, fc2, dtype=torch.float32)
         self.output = nn.Linear(fc2, output_size, dtype=torch.float32)
